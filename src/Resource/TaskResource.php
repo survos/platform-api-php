e551dbf9bbcd51bff9eda17e03c4ca1bc52c616f
Piotr Grochowski <piogrek@gmail.com>
2015-11-20 17:29:35 +0000

    tasks resource

---
+++ src/Resource/TaskResource.php
@@ -0,0 +1,16 @@
+<?php
+namespace Survos\Client\Resource;
+
+use Survos\Client\Resource\Helper\DeleteByIdHelper;
+use Survos\Client\Resource\Helper\GetByIdHelper;
+use Survos\Client\Resource\Helper\GetListHelper;
+use Survos\Client\Resource\Helper\GetOneByFieldHelper;
+use Survos\Client\Resource\Helper\SaveHelper;
+
+class TaskResource extends BaseResource
+{
+    use SaveHelper, GetListHelper, GetByIdHelper, DeleteByIdHelper, GetOneByFieldHelper;
+
+    /** @var string */
+    protected $resource = 'tasks';
+}
