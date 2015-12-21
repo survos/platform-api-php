706b4bd52854618d780bd9b7eafd2bd086442d9d
Keith C. Ivey <keith@iveys.org>
2015-12-20 22:00:12 -0500

    add GetByCodeHelper to TrackingAppResource

--- src/Resource/TrackingAppResource.php
+++ src/Resource/TrackingAppResource.php
@@ -2,11 +2,12 @@
 
 namespace Survos\Client\Resource;
 
+use Survos\Client\Resource\Helper\GetByCodeHelper;
 use Survos\Client\Resource\Helper\GetListHelper;
 
 class TrackingAppResource extends BaseResource
 {
-    use GetListHelper;
+    use GetListHelper, GetByCodeHelper;
 
     /** @var string */
     protected $resource = 'tracking-apps';
